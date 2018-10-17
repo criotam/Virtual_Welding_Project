8a12bec1da4a1fa17667876067416bf165eaaa53
pratikbanjare <jay.lelouch@outlook.com>
2018-10-17 12:22:38 +0530

    Level Design

---
+++ Arc Welding/Assets/Scripts/GunMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMovement : MonoBehaviour {
+	public float speed = 1.0F;
+	// Update is called once per frame
+	void Update () {
+		float translationY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		float translationX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		transform.Translate(translationX ,translationY ,0);
+	}
+}
