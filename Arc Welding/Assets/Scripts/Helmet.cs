ce47b8b0353164f767c027584d0536752475f5bb
pratikbanjare <jay.lelouch@outlook.com>
2018-10-29 14:14:48 +0530

    Gameplay Design And Controller

--- Arc Welding/Assets/Scripts/Helmet.cs
+++ Arc Welding/Assets/Scripts/Helmet.cs
@@ -1,22 +1,17 @@
 ﻿using UnityEngine;
 
 public class Helmet : MonoBehaviour {
-    //public Vector3 pos;
-    //public Quaternion rot;
-    //public float distance;
+
+    public GameObject HelmetPosition;
     public float Distance;
     Controls control;
     bool pointingTo;
     bool HelmetOn;
     void Start()
     {
-        
         pointingTo = false;
         HelmetOn = false;
         control = GameObject.FindGameObjectWithTag("Controls").GetComponent<Controls>();
-        //pos = this.transform.position;
-        //rot = this.transform.rotation;
-        //distance = Vector3.Distance(pos, GameObject.FindGameObjectWithTag("Player").transform.position);
     }
 
 	void Update()
@@ -24,31 +19,26 @@ public class Helmet : MonoBehaviour {
         
         if (Input.GetButtonDown("Fire1") && Controls.CurrentState == "EmptyHand" && pointingTo == true)
         {
-            control.CHangeState(1);
+            Controls.SwitchToHelmetOn();
             Debug.Log(Controls.CurrentState);
             HelmetOn = true;
             gameObject.GetComponentInChildren<MeshCollider>().enabled = false;
         }
         else if (Input.GetButtonDown("Fire3") && Controls.CurrentState == "HelmetOn")
         {
-            control.CHangeState(0);
+            Controls.SwitchToEmptyhand();
+            this.gameObject.transform.position = HelmetPosition.transform.position;
+            this.gameObject.transform.rotation = HelmetPosition.transform.rotation;
             HelmetOn = false;
             gameObject.GetComponentInChildren<MeshCollider>().enabled = true;
+            Debug.Log(Controls.CurrentState);
         }
         if (HelmetOn)
         {
             this.transform.rotation = Camera.main.transform.rotation;
             this.transform.position = Camera.main.transform.position + Camera.main.transform.forward * Distance;
         }
-    }
 
-    void FixedUpdate()
-    {
-        //this.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 0.1025671f;
-        //pos = this.transform.position;
-        //rot = this.transform.rotation;
-        //distance = Vector3.Distance(pos, GameObject.FindGameObjectWithTag("Player").transform.position);
-        
     }
 
     public void OnPointerEnter()
@@ -60,4 +50,12 @@ public class Helmet : MonoBehaviour {
         pointingTo = false;
     }
 
+    public void ToggleView()
+    {
+        MeshRenderer mr = GetComponentInChildren<BoxCollider>().gameObject.GetComponent<MeshRenderer>();
+        if (mr.enabled == true)
+            mr.enabled = false;
+        else
+            mr.enabled = true;
+    }
 }
---
+++ Arc Welding/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public float Distance;
+    public GameObject menu;
+    bool MenuActive = false;
+    string PreviousState;
+
+    void Start()
+    {
+        menu.SetActive(MenuActive);
+    }
+
+    void Update()
+    {
+        if (Controls.CurrentState != "Welding" && Controls.CurrentState != "CloseToTable")
+        {
+            if (Input.GetButtonDown("Cancel") || Input.GetButtonDown("Jump"))
+            {
+                if (MenuActive)
+                {
+                    MenuActive = false;
+                    Controls.SwitchTo(PreviousState);
+                }
+                else
+                {
+                    MenuActive = true;
+                    PreviousState = Controls.CurrentState;
+                    Controls.SwitchToPauseMenu();
+                }
+                menu.SetActive(MenuActive);
+            }
+        }
+        if (MenuActive)
+        {
+            menu.transform.rotation = new Quaternion(0, Camera.main.transform.rotation.y,0, Camera.main.transform.rotation.w);
+            menu.transform.position = Camera.main.transform.position + new Vector3 (Camera.main.transform.forward.x,0,Camera.main.transform.forward.z )* Distance;
+        }
+
+    }
+
+    public void ReturnToGame()
+    {
+        MenuActive = false;
+        Controls.SwitchTo(PreviousState);
+        menu.SetActive(MenuActive);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
